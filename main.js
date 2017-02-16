e729ca5c3dc1b63aaa476f3a3770af7dffe43312
Michael Martinez <martinez1212@gmail.com>
2017-02-16 12:30:46 -0800

    refactored code

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-/*jshint esversion: 6 */
-
 let brickBreak = new Phaser.Game(800, 600, Phaser.CANVAS, '', {
     preload,
     create,
@@ -31,7 +29,6 @@ let musicOn = false;
 let pause_label;
 let menu;
 
-
 function preload() {
     brickBreak.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     brickBreak.scale.pageAlignHorizontally = true;
@@ -58,11 +55,9 @@ function create() {
     brickBreak.physics.startSystem(Phaser.Physics.ARCADE);
     brickBreak.physics.arcade.checkCollision.down = false;
     regWidth = brickBreak.world.width * 0.5;
-
     start = brickBreak.add.button(regWidth, 300, "start", gameStart);
     start.scale.setTo(0.8);
     start.anchor.set(0.5);
-
     ball = brickBreak.add.sprite(regWidth, 514, "ball");
     brickBreak.physics.enable(ball, Phaser.Physics.ARCADE);
     ball.anchor.set(0.5);
@@ -71,38 +66,31 @@ function create() {
     ball.checkWorldBounds = true;
     ball.body.bounce.set(1.02);
     ball.events.onOutOfBounds.add(lostBall, this);
-
     paddle = brickBreak.add.sprite(regWidth, 560, "paddle");
     brickBreak.physics.enable(paddle, Phaser.Physics.ARCADE);
     paddle.anchor.set(0.5, 1);
-    paddle.scale.setTo(0.4,0.4);
+    paddle.scale.setTo(0.4, 0.4);
     paddle.body.collideWorldBounds = true;
     paddle.body.immovable = true;
-
     paddle.body.onCollide = new Phaser.Signal();
     paddle.body.onCollide.add(soundPaddles, this);
     if (gameOverBtn) {
-      gameOverBtn.destroy();
+        gameOverBtn.destroy();
     }
     textMaker();
     fieldMaker();
 }
 
 function update() {
-  brickBreak.physics.arcade.collide(ball, paddle, paddleBallCollision);
-  brickBreak.physics.arcade.collide(ball, brickField, destroyBrick);
-  if (paddleMvmt) {
-    if (brickBreak.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-      paddle.x -=  16;
-    } else if (brickBreak.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-      paddle.x += 16;
+    brickBreak.physics.arcade.collide(ball, paddle, paddleBallCollision);
+    brickBreak.physics.arcade.collide(ball, brickField, destroyBrick);
+    if (paddleMvmt) {
+        if (brickBreak.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+            paddle.x -= 16;
+        } else if (brickBreak.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+            paddle.x += 16;
+        }
     }
-    // else {
-    //
-    //   // paddle.x = brickBreak.input.x || regWidth;
-    // }
-  }
-
 }
 
 function textMaker() {
@@ -113,29 +101,34 @@ function textMaker() {
     textScore = brickBreak.add.text(630, 20, `Score: ${score}`, textInfo);
     highScore = brickBreak.add.text(630, 65, `HighScore: ${highScoreVal}`, textInfo);
     livesText = brickBreak.add.text(20, 20, `Lives: ${lives}`, textInfo);
-
     roundText = brickBreak.add.text(regWidth, 400, 'Ready?', textInfo);
     roundText.anchor.set(0.5);
     roundText.visible = false;
-
     pause_label = brickBreak.add.text(20, 65, 'Pause', textInfo);
     pause_label.inputEnabled = true;
     pause_label.events.onInputUp.add(() => {
-      brickBreak.paused = true;
-      menu = brickBreak.add.sprite(regWidth, 300, 'paused');
-      menu.anchor.set(0.5, 0.5);
-      menu.scale.setTo(0.8);
-      brickBreak.input.onDown.add(unpause, self);
+        brickBreak.paused = true;
+        menu = brickBreak.add.sprite(regWidth, 300, 'paused');
+        menu.anchor.set(0.5, 0.5);
+        menu.scale.setTo(0.8);
+        brickBreak.input.onDown.add(unpause, self);
     });
 }
 
 function unpause() {
-  brickBreak.paused = false;
-  menu.visible = false;
+    brickBreak.paused = false;
+    menu.visible = false;
 }
 
 function fieldMaker() {
-    brickStyle = {padding: 10, width: 30, offset: {top: 50,left: 160}};
+    brickStyle = {
+        padding: 10,
+        width: 30,
+        offset: {
+            top: 50,
+            left: 160
+        }
+    };
     brickField = brickBreak.add.group();
     for (let y = 0; y < 4; y++) {
         for (let x = 0; x < 12; x++) {
@@ -154,26 +147,25 @@ function fieldMaker() {
 
 function paddleBallCollision(ball, paddle) {
     ball.body.velocity.x = -2 * 5 * (paddle.x - ball.x);
-    // ball.body.velocity.set(500, -500);
 }
 
 function gameStart() {
     start.destroy();
     paddleMvmt = true;
-    if(!musicOn) {
-    music = brickBreak.add.audio('funk');
-    music.play();
-  }
+    if (!musicOn) {
+        music = brickBreak.add.audio('funk');
+        music.play();
+    }
     releaseBall();
 }
 
 function releaseBall() {
-  roundText.visible = false;
-  paddleMvmt = true;
-  if (ballOnPaddle) {
-    ball.body.velocity.set(500, -500);
-    ballOnPaddle = false;
-  }
+    roundText.visible = false;
+    paddleMvmt = true;
+    if (ballOnPaddle) {
+        ball.body.velocity.set(500, -500);
+        ballOnPaddle = false;
+    }
 }
 
 function destroyBrick(ball, innerBricks) {
@@ -185,10 +177,10 @@ function destroyBrick(ball, innerBricks) {
 
 function checkWin() {
     if (brickField.countLiving() === 0) {
-      gameWin = brickBreak.add.button(regWidth, 300, "gameWin", restart);
-      ball.destroy();
-      gameWin.scale.setTo(0.8);
-      gameWin.anchor.set(0.5);
+        gameWin = brickBreak.add.button(regWidth, 300, "gameWin", restart);
+        ball.destroy();
+        gameWin.scale.setTo(0.8);
+        gameWin.anchor.set(0.5);
     }
 }
 
@@ -204,32 +196,31 @@ function lostBall(ball) {
         brickBreak.time.events.add(Phaser.Timer.SECOND * 2.2, releaseBall, this);
     } else {
         restart(ball);
-
     }
 }
 
 function restart(ball) {
-  musicOn = true;
-  sndBrick.stop();
-  sndPaddle.stop();
-  ballOnPaddle = true;
-  lives = 3;
-  ball.destroy();
-  paddle.destroy();
-  textScore.destroy();
-  highScore.destroy();
-  livesText.destroy();
-  pause_label.destroy();
-  if (score > highScoreVal) {
-    highScoreVal = score;
-  }
-  window.localStorage.setItem("HighScore", highScoreVal);
-  score = 0;
-  roundText.destroy();
-  brickField.destroy();
-  gameOverBtn = brickBreak.add.button(regWidth, 300, "gameOverBtn", create);
-  gameOverBtn.scale.setTo(0.8);
-  gameOverBtn.anchor.set(0.5);
+    musicOn = true;
+    sndBrick.stop();
+    sndPaddle.stop();
+    ballOnPaddle = true;
+    lives = 3;
+    ball.destroy();
+    paddle.destroy();
+    textScore.destroy();
+    highScore.destroy();
+    livesText.destroy();
+    pause_label.destroy();
+    if (score > highScoreVal) {
+        highScoreVal = score;
+    }
+    window.localStorage.setItem("HighScore", highScoreVal);
+    score = 0;
+    roundText.destroy();
+    brickField.destroy();
+    gameOverBtn = brickBreak.add.button(regWidth, 300, "gameOverBtn", create);
+    gameOverBtn.scale.setTo(0.8);
+    gameOverBtn.anchor.set(0.5);
 }
 
 function paddleReset() {
@@ -238,7 +229,7 @@ function paddleReset() {
 }
 
 function soundPaddles() {
-  sndPaddle = brickBreak.add.audio('clash');
+    sndPaddle = brickBreak.add.audio('clash');
     if (!ballOnPaddle) {
         sndPaddle.play();
     }
